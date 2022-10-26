e2ae5268b06c73149858fa2b84a97a9b330a42d2
GranSpencer09 <Granspencer09@gmail.com>
2022-10-26 11:48:20 -0700

    added basic get route to load stored notes

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const path = require("path");
+const notes = require("./db/db.json");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Neccessary middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Get homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// Get notes page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// Read the db.json file and return all saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+// Receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+app.post("api/notes", (req, res) => {});
 
 app.listen(PORT, () => console.log(`App listing on port ${PORT}`));
